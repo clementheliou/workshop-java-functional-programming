e40f1bc33e4a373ff8a7d1508e7e22b58fc1dae5
Clément HELIOU <cheliou@xebia.fr>
2015-02-17 21:18:45 +0100

    feat(list): add starting state

---
+++ src/test/java/fr/xebia/workshop/java/functionalprogramming/ListTest.java
@@ -0,0 +1,24 @@
+package fr.xebia.workshop.java.functionalprogramming;
+
+public class ListTest {
+
+    // Exercise 1
+
+//    @Test
+//    public void should_provide_an_empty_element() {
+//        final List emptyElement = Nil.INSTANCE;
+//    }
+//
+//    @Test
+//    public void should_provide_a_single_argument_constructor() {
+//        new Cons<>(2);
+//    }
+//
+//
+//    @Test
+//    public void should_provide_a_bi_argument_constructor() {
+//
+//        @SuppressWarnings("unchecked")
+//        final List<Integer> aList = new Cons<>(2, Nil.INSTANCE);
+//    }
+}
